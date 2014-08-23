aa07195984b2d284d42192c36ecfc1a7b7ce0e89
sprawls <alex3521@hotmail.fr>
2014-08-23 18:54:11 -0400

    day manager created

---
+++ Ludum Dare 30/Assets/DayManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayManager : MonoBehaviour {
+
+	/// <summary>
+	/// This is basicly the stats for each Level. Does nothing but store data. 
+	/// </summary>
+
+	public static int[] foodQuota; //Food
+	public static int[] weaponQuota; //Weapon
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
